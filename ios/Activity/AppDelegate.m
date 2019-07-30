6f1b7fb5aad2ff26c48cd0081b5ce938524d3c93
Benjamin Pollard <Benpollard1990@gmail.com>
2019-07-30 17:23:09 +0100

    init for iOS

--- ios/Activity/AppDelegate.m
+++ ios/Activity/AppDelegate.m
@@ -15,26 +15,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.reactBridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    //self.reactBridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
-    RCTDevMenuItem *devMenuItem = [RCTDevMenuItem buttonItemWithTitle:@"Custom menu item"
-                                                              handler:^{
-                                                                  NSLog(@"Custom menu item clicked!");
-                                                              }];
-    [self.reactBridge.devMenu addItem:devMenuItem];
+//    RCTDevMenuItem *devMenuItem = [RCTDevMenuItem buttonItemWithTitle:@"Custom menu item"
+//                                                              handler:^{
+//                                                                  NSLog(@"Custom menu item clicked!");
+//                                                              }];
+//    [self.reactBridge.devMenu addItem:devMenuItem];
 
-    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:self.reactBridge
-                                                     moduleName:@"ActivityDemoComponent"
-                                              initialProperties:nil];
-    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-    UIViewController *rootViewController = [UIViewController new];
-    rootViewController.view = rootView;
-    navigationController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
-
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.rootViewController = navigationController;
-    [self.window makeKeyAndVisible];
+//    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:self.reactBridge
+//                                                     moduleName:@"ActivityDemoComponent"
+//                                              initialProperties:nil];
+//    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+//
+//    UIViewController *rootViewController = [UIViewController new];
+//    rootViewController.view = rootView;
+//    navigationController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
+//
+//    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//    self.window.rootViewController = navigationController;
+//    [self.window makeKeyAndVisible];
 
     return YES;
 }
@@ -59,7 +59,7 @@
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
-    NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.android"
+    NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios"
                                                                            fallbackResource:nil];
     return jsCodeLocation;
 }
