9a83fbe76fbb346f1a4b9e7297f2e160b5710ceb
Benjamin Pollard <Benpollard1990@gmail.com>
2019-07-31 10:04:27 +0100

    fix ios first view controller not showing

---
+++ ios/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Activity
+//
+//  Created by benjamin pollard on 30/07/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MainViewController: UIViewController {
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+}
